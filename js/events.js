4cb2bc4b80eeca3183483f7ef51ee27df73e2db1
vatiza <rakibur.mac@gmail.com>
2023-02-15 02:17:24 +0600

    addc olor goldenrod

--- js/events.js
+++ js/events.js
@@ -15,3 +15,18 @@ const makePurpleBtn=document.getElementById('make-purple');
 makePurpleBtn.onclick= function makePurple() {
     document.body.style.backgroundColor = 'purple'
 }
+
+//! Option 4
+const pinkBtn=document.getElementById('make-pink');
+pinkBtn.addEventListener('click',makePink)
+ function makePink(){
+    document.body.style.backgroundColor='pink'
+ }
+
+//!  option 4 another
+const makeGreenBtn=document.getElementById('make-green');
+makeGreenBtn.addEventListener('click',function makeGreen(){
+    document.body.style.backgroundColor='green';
+})
+
+//! Option 4 finally
